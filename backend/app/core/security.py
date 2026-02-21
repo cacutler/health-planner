728304f114cc2b7c14e1ec9700bfed27867d7564
cacutler <116305038+cacutler@users.noreply.github.com>
2026-02-21 16:56:58 -0700

    Implemented some password security

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,4 +1,17 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, UTC
 from jose import jwt
 from passlib.context import CryptContext
 import os
+SECRET_KEY = os.getenv("SECRET_KET", "dev-secret")
+ALGORITHM = "HS256"
+ACCESS_TOEKN_EXPIRE_MINUTES = 30
+pwd_context = CryptContext(schemes=["bcrypt"], depreated="auto")
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+def verify_password(password: str, hashed: str) -> bool:
+    return pwd_context.verify(password, hashed)
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(UTC) + timedelta(minutes=ACCESS_TOEKN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
